84e1bdee1f1b4d96bdd2c5656e1b4ca7cb7ef333
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-08 08:57:03 +0900

    5/8 start

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,15 +4,20 @@ class StaticPagesController < ApplicationController
   	if signed_in?
   		@micropost = current_user.microposts.build
   		@feed_items = current_user.feed.paginate(page: params[:page])
+    else
+      render :layout => "second_layout"
   	end
   end
 
   def help
+    render :layout => "second_layout"
   end
 
   def about
+    render :layout => "second_layout"
   end
 
   def contact
+    render :layout => "second_layout"
   end
 end
