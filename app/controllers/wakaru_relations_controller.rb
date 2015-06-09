dcbc7395e4a4c30493cc922e545b58cb748f4a4e
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-09 11:01:58 +0900

    add wakaru wo Ajax nor design

--- app/controllers/wakaru_relations_controller.rb
+++ app/controllers/wakaru_relations_controller.rb
@@ -4,19 +4,20 @@ class WakaruRelationsController < ApplicationController
   def create
     @wakarareru = Micropost.find(params[:wakaru_relation][:wakarareru_post_id])
     logger.debug "------------------------------VVV----------------------------------"
-    logger.debug wakarareru_post_id
+    # logger.debug wakarareru_post_id
     current_user.wakaru!(@wakarareru)
+    logger.debug "done"
     # redirect_to root_path
     respond_to do |format|
         format.html { redirect_to root_path }
         format.js
     end
+    logger.debug "complete"
   end
 
   def destroy
-    # @wakarareru = WakaruRelation.find(params[:id]).wakarareru_post
-    # current_user.wakaranai!(@wakarareru)
-    # redirect_to root_path
+    @wakarareru = WakaruRelation.find(params[:id]).wakarareru_post
+    current_user.wakaranai!(@wakarareru)
     respond_to do |format|
       format.html { redirect_to root_path }
       format.js
