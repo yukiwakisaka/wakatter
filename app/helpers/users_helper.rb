a338c5714af0f9e1b753584ff563803fdc186693
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-13 03:41:44 +0900

    fin wakaru_post view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,15 +7,23 @@ module UsersHelper
 	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
 	end
 
-	def icon_size(post_count, wakaru_count)
-		size = 10 + Math.log(Math.sqrt(post_count)) * 20
-		if size > 40
-			size = 40
+	def icon_size(item)
+		size_post = 10 + Math.log(Math.sqrt(item.user.microposts.count)) * 5
+		if size_post > 50
+			size_post = 50
 		end
-		size = size + wakaru_count * 1
-		if size > 70
-			size = 70
+		
+		size_wakaru =  item.wakaru_users.count * 5
+		if size_wakaru > 50
+			size_wakaru = 50
 		end
+
+		size_ff = (item.user.followers.count + item.user.followed_users.count) * 0.2
+		if size_ff > 20
+			size_ff = 20 
+		end
+
+		size = size_post + size_wakaru + size_ff
 		return size
 	end
 end
