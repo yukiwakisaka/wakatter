82ab67333cae142b653cd12ba536e648642d9c1b
脇坂 友貴 <yuki.wakisaka@bizreach.co.jp>
2015-06-16 23:23:12 +0900

    grav

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,8 +4,7 @@ module UsersHelper
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 	    size = options[:size]
 	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-	    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-	    alt = ""
+	    image_tag(gravatar_url, alt: "", class: "gravatar")
 	end
 
 	def icon_size(item)
